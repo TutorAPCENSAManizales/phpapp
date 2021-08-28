95adca0a7e2600c4934532adf441d343e3ae213f
Daniel Fernando Rivera Cubillos <daniel.rivera@pragma.com.co>
2021-08-28 09:36:51 -0500

    Crear rutas para la aplicación

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::view('/', 'home');
+Route::view('/login', 'login');
+Route::view('/signin', 'signin');
+Route::view('/pokedex', 'pokedex');
+Route::view('/pokemon', 'pokemon');
